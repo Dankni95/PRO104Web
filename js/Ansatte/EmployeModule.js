9b71a300ab1b2740dc173022c38f64391719a29c
StianOek <66445281+StianOek@users.noreply.github.com>
2021-05-21 12:22:43 +0200

    9. trying out module

---
+++ js/Ansatte/EmployeModule.js
@@ -0,0 +1,25 @@
+
+const EmployeModule = (function () {
+
+    const employe = [
+    {
+        image: "person-1.jpg",
+        name: "Kalle balle",
+        phone: "+47 30 30 30 03",
+        title: "Admin",
+        category: "Avdeling lambertseter"
+    },
+
+    {
+        image: "person-1.jpg",
+        name: "Kalle balle",
+        phone: "+47 30 30 30 03",
+        title: "Admin",
+        category: "Avdeling frogner"
+    },
+];
+
+const getAllEmployees = () => employe;
+return {getAllEmployees}
+
+} ());
---
+++ js/Ansatte/employe.js
@@ -0,0 +1,8 @@
+import EmployeModule from './EmployeModule.js'
+
+const cardSectionDiv = document.querySelectorAll("#card-section");
+
+
+const department = () => {
+    
+}
