774cd6549b2153eb81d67694ac01bda9d5dc3e5e
Daniel Lysak <Daniel_kn@live.no>
2021-05-27 12:42:37 +0200

    added reading and writing data from JSON

--- js/menyModules/MainModule.js
+++ js/menyModules/MainModule.js
@@ -1,44 +1,49 @@
 import DataModule from "../globalModules/DataModule.js";
 const MainModule = (function data() {
 
-//modal
-
-const editDish = document.getElementsByTagName('BUTTON');
-const modalBackground = document.querySelector('.modal-background');
-const modal = document.querySelector('.modal');
-const saveButton = document.getElementById('save-button');
-const featureAlert = document.getElementById('horizontal-id');
-
-//Adds is-active to the modal class.
-for (var i = 0; i < editDish.length; i++) {
-    if(editDish[i].className === 'button active'){ editDish[i].addEventListener('click',  function(e) {
-        modal.classList.add('is-active');
-      });}
-};
-
-//Removes is-active from the modal class when clicked on background.'
-modalBackground.addEventListener('click', () => {
-    modal.classList.remove('is-active');
-});
-
-console.log(editDish.className);
-
-//Makes save button lock the item
-saveButton.addEventListener('click', () => {
-    modal.classList.remove('is-active');
-});
-
-featureAlert.addEventListener('click', () => {
-    alert("Denne funksjonen er enda ikke implementert, vennligst kontakt systemansvarlig for mer informasjon. ");
-});
 
 
 }());
 
+function addEventsToButtons() {
+
+    //modal
+
+    const editDish = document.getElementsByTagName('BUTTON');
+    const modalBackground = document.querySelector('.modal-background');
+    const modal = document.querySelector('.modal');
+    const saveButton = document.getElementById('save-button');
+    const featureAlert = document.getElementById('horizontal-id');
+
+    //Adds is-active to the modal class.
+    for (var i = 0; i < editDish.length; i++) {
+        if (editDish[i].className === 'button active') {
+            editDish[i].addEventListener('click', function (e) {
+                modal.classList.add('is-active');
+            });
+        }
+    };
+
+    //Removes is-active from the modal class when clicked on background.'
+    modalBackground.addEventListener('click', () => {
+        modal.classList.remove('is-active');
+    });
+
+
+    //Makes save button lock the item
+    saveButton.addEventListener('click', () => {
+        modal.classList.remove('is-active');
+    });
+
+    featureAlert.addEventListener('click', () => {
+        alert("Denne funksjonen er enda ikke implementert, vennligst kontakt systemansvarlig for mer informasjon. ");
+    });
+}
 
 
 
-export default MainModule;
+
+export default {MainModule, addEventsToButtons};
 
 
 
--- js/menyModules/indexMeny.js
+++ js/menyModules/indexMeny.js
@@ -1,3 +1,4 @@
 import DataModule from "../globalModules/DataModule.js";
 import MainModule from "./MainModule.js";
+import MenyModule from "./MenyModule.js";
     
